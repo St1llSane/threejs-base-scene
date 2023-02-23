959f3f2eb5177e9a7fb8fa7708591e9381876345
Alexander Vetrov <notescape262@gmail.com>
2023-02-23 21:25:48 +0200

    base scene

--- main.js
+++ main.js
@@ -1,26 +1,97 @@
 import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import * as dat from 'dat.gui'
 import './style.css'
 
+// Canvas
+const canvas = document.querySelector('.webgl')
+
+// Scene
 const scene = new THREE.Scene()
 
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 'yellow' })
-const cube = new THREE.Mesh(geometry, material)
-scene.add(cube)
+// Material
+const material = new THREE.MeshStandardMaterial()
 
-const size = {
-  width: 800,
-  height: 600,
+// Mesh
+const plane = new THREE.Mesh(new THREE.PlaneGeometry(10, 10), material)
+plane.rotation.set(-Math.PI / 2, 0, 0)
+plane.receiveShadow = true
+scene.add(plane)
+
+const sphere = new THREE.Mesh(new THREE.SphereGeometry(1, 64, 64), material)
+sphere.position.set(0, 1, 0)
+sphere.castShadow = true
+scene.add(sphere)
+
+// Light
+const ambientLight = new THREE.AmbientLight(0xffffff)
+ambientLight.intensity = 0.5
+scene.add(ambientLight)
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5)
+directionalLight.position.set(-2, 1, 0)
+directionalLight.castShadow = true
+directionalLight.shadow.mapSize.width = 2048
+directionalLight.shadow.mapSize.height = 2048
+scene.add(directionalLight)
+
+// Sizes
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
 }
 
-const camera = new THREE.PerspectiveCamera(75, size.width / size.height)
-camera.position.z = 3
+// Camera
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100
+)
+camera.position.x = 0
+camera.position.y = 6
+camera.position.z = 6
 scene.add(camera)
 
-const canvas = document.querySelector('.webgl')
-const renderer = new THREE.WebGLRenderer({
-  canvas,
-})
-renderer.setSize(size.width, size.height)
-
+// Renderer
+const renderer = new THREE.WebGLRenderer({ canvas })
+renderer.setSize(sizes.width, sizes.height)
+renderer.shadowMap.enabled = true
+renderer.shadowMap.type = THREE.PCFSoftShadowMap
 renderer.render(scene, camera)
+
+// Resizing
+window.addEventListener('resize', () => {
+  sizes.width = window.innerWidth
+  sizes.height = window.innerHeight
+
+  camera.aspect = sizes.width / sizes.height
+  camera.updateProjectionMatrix()
+
+  renderer.setSize(sizes.width, sizes.height)
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+controls.dampingFactor = 0.03
+controls.minDistance = 1
+controls.maxDistance = 10
+
+// Debug UI
+const gui = new dat.GUI()
+gui.closed = true
+
+// Animations
+const clock = new THREE.Clock()
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime()
+
+  // Update controls
+  controls.update()
+
+  renderer.render(scene, camera)
+  window.requestAnimationFrame(tick)
+}
+tick()
